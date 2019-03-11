3b2142e24589e676fef97e9a27f0cf18645afcab
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2019-03-11 10:59:11 +0100

    Apply pep8

--- lasso_solutions.py
+++ lasso_solutions.py
@@ -2,99 +2,104 @@
 import numpy as np
 from numpy import linalg as la
 import matplotlib.pyplot as plt
-import my_toolbox.sparsity as sparse 
+import my_toolbox.sparsity as sparse
 import my_toolbox.forwardbackward as fb
 
-###### This is for production only ######
+#########################################
+# This is for production only
 import importlib
-importlib.reload(sparse) 
-importlib.reload(fb) 
+importlib.reload(sparse)
+importlib.reload(fb)
 #########################################
 
-np.random.seed(seed=78) # Seed for np.random
-dpi = 230 # Resolution used for plotting (230 for small screen, 100 for large one)
+np.random.seed(seed=78)  # Seed for np.random
+dpi = 230  # Resolution for plotting (230 for small screen, 100 for large one)
 
-# We start by defining the characteristics of the problem: dimensions, sparsity level, etc.
-data_size = 100 
-data_number = round(data_size/2)
+# We start by defining the characteristics of the problem
+data_size = 100
+data_number = round(data_size / 2)
 sparsity_level = 10
-noise_level = 1e-2*0
+noise_level = 1e-2 * 0
 
 # We define the main components of our problem
-Phi = np.random.randn(data_number,data_size)
-x0 = np.sign(sparse.randn(data_size,1,sparsity_level))
-noise = noise_level * np.random.randn(data_number,1)
+Phi = np.random.randn(data_number, data_size)
+x0 = np.sign(sparse.randn(data_size, 1, sparsity_level))
+noise = noise_level * np.random.randn(data_number, 1)
 y = Phi@x0 + noise
 
 # Let's compare the ground truth with the pseudo inverse solution
-x_pinv = la.lstsq(Phi,y,rcond=None)[0]
+x_pinv = la.lstsq(Phi, y, rcond=None)[0]
 _ = plt.figure(dpi=dpi)
-sparse.stem(x0,"C0","ground truth")
-sparse.stem(x_pinv,"C1","pinv solution")
+sparse.stem(x0, "C0", "ground truth")
+sparse.stem(x_pinv, "C1", "pinv solution")
 plt.show()
 
-# Let's compare the ground truth with the solution of the LASSO (computed with the Forward-Backward algorithm)
+# Let's compare the ground truth with the solution of the LASSO
+# (computed with the Forward-Backward algorithm)
 reg_param = 0.01
 iter_nb = 40000
 
 x_reg = fb.lasso(Phi, y, reg_param, iter_nb)
 _ = plt.figure(dpi=dpi)
-sparse.stem(x0,"C0","ground truth")
-sparse.stem(x_reg,"C1","reg solution")
+sparse.stem(x0, "C0", "ground truth")
+sparse.stem(x_reg, "C1", "reg solution")
 plt.show()
 
 # We look at what happens during the iterations of the algorithm
 x_reg, details = fb.lasso(Phi, y, reg_param, iter_nb, verbose=True)
-plt.figure(dpi=dpi); plt.title(r"Evolution of $f(x_n)$")
-plt.plot(details.get("function_value")) 
-plt.figure(dpi=dpi); plt.title(r"Evolution of supp$(x_n)$")
+plt.figure(dpi=dpi)
+plt.title(r"Evolution of $f(x_n)$")
+plt.plot(details.get("function_value"))
+plt.figure(dpi=dpi)
+plt.title(r"Evolution of supp$(x_n)$")
 plt.plot(details.get("iterate_support"))
 plt.show()
 
-# Now we generate and save images corresponding to the regularized solutions for various values for reg_param
+# Now we generate and save images corresponding to
+# the regularized solutions for various values for reg_param
 # Quite expensive in time depending on the parameters!
 _ = plt.figure(dpi=dpi)
-for reg_param in np.round(np.linspace(10.1,100,900),1):
+for reg_param in np.round(np.linspace(10.1, 100, 900), 1):
     x_reg = fb.lasso(Phi, y, reg_param, iter_nb=200)
-    _=plt.title(r"Regularisation parameter $\lambda=$"+str(reg_param))
-    sparse.stem(x0,"C0","ground truth")
-    sparse.stem(x_reg,"C1","reg solution")
-    plt.savefig('output/L1_reg/reg_sol_'+str(reg_param)+'.png',bbox_inches='tight')
+    _ = plt.title(r"Regularisation parameter $\lambda=$" + str(reg_param))
+    sparse.stem(x0, "C0", "ground truth")
+    sparse.stem(x_reg, "C1", "reg solution")
+    plt.savefig('output/L1_reg/reg_sol_' + str(reg_param) + '.png',
+                bbox_inches='tight')
     plt.clf()
 print("done")
-for reg_param in np.round(np.linspace(1.1,10,90),1):
+for reg_param in np.round(np.linspace(1.1, 10, 90), 1):
     x_reg = fb.lasso(Phi, y, reg_param, iter_nb=1000)
-    _=plt.title(r"Regularisation parameter $\lambda=$"+str(reg_param))
-    sparse.stem(x0,"C0","ground truth")
-    sparse.stem(x_reg,"C1","reg solution")
-    plt.savefig('output/L1_reg/reg_sol_'+str(reg_param)+'.png',bbox_inches='tight')
+    _ = plt.title(r"Regularisation parameter $\lambda=$" + str(reg_param))
+    sparse.stem(x0, "C0", "ground truth")
+    sparse.stem(x_reg, "C1", "reg solution")
+    plt.savefig('output/L1_reg/reg_sol_' + str(reg_param) + '.png',
+                bbox_inches='tight')
     plt.clf()
 print("done")
-for reg_param in np.round(np.linspace(0.11,1,9),1):
+for reg_param in np.round(np.linspace(0.11, 1, 9), 1):
     x_reg = fb.lasso(Phi, y, reg_param, iter_nb=3000)
-    _=plt.title(r"Regularisation parameter $\lambda=$"+str(reg_param))
-    sparse.stem(x0,"C0","ground truth")
-    sparse.stem(x_reg,"C1","reg solution")
-    plt.savefig('output/L1_reg/reg_sol_'+str(reg_param)+'.png',bbox_inches='tight')
+    _ = plt.title(r"Regularisation parameter $\lambda=$" + str(reg_param))
+    sparse.stem(x0, "C0", "ground truth")
+    sparse.stem(x_reg, "C1", "reg solution")
+    plt.savefig('output/L1_reg/reg_sol_' + str(reg_param) + '.png',
+                bbox_inches='tight')
     plt.clf()
 print("done")
 
 # Super super expensive
-for reg_param in np.round(np.linspace(0.11,100,999),1):
+for reg_param in np.round(np.linspace(0.11, 100, 999), 1):
     if reg_param < 1:
-        iter_nb=3000
+        iter_nb = 3000
     elif reg_param < 10:
         iter_nb = 1000
     else:
         iter_nb = 200
     x_reg = fb.lasso(Phi, y, reg_param, iter_nb)
-    _=plt.title(r"Regularisation parameter $\lambda=$"+str(reg_param))
-    sparse.stem(x0,"C0","ground truth")
-    sparse.stem(x_reg,"C1","reg solution")
-    plt.savefig('output/L1_reg/reg_sol_'+str(reg_param)+'.png',bbox_inches='tight')
+    _ = plt.title(r"Regularisation parameter $\lambda=$" + str(reg_param))
+    sparse.stem(x0, "C0", "ground truth")
+    sparse.stem(x_reg, "C1", "reg solution")
+    plt.savefig('output/L1_reg/reg_sol_' + str(reg_param) + '.png',
+                bbox_inches='tight')
     plt.clf()
 print("done")
-
-
-
-
--- lasso_solutions_projected.py
+++ lasso_solutions_projected.py
@@ -1,136 +1,155 @@
 import numpy as np
 from numpy import linalg as la
 import matplotlib.pyplot as plt
-import my_toolbox.sparsity as sparse 
+import my_toolbox.sparsity as sparse
 import my_toolbox.forwardbackward as fb
 
-
-np.random.seed(seed=74) # Seed for np.random (78)
-dpi = 230 # Resolution used for plotting (230 for small screen, 100 for large one)
+np.random.seed(seed=74)  # Seed for np.random (78)
+dpi = 100  # Resolution for plotting (230 for small screen, 100 for large one)
 plt.ion()
 
-# We start by defining the characteristics of the problem: dimensions, sparsity level, etc.
-data_size = 100 
-data_number = round(data_size/2)
+# We start by defining the characteristics of the problem
+data_size = 100
+data_number = round(data_size / 2)
 sparsity_level = 10
 
 # We define the main components of our problem
-Phi = np.random.randn(data_number,data_size)
-x0 = np.sign(sparse.randn(data_size,1,sparsity_level))
+Phi = np.random.randn(data_number, data_size)
+x0 = np.sign(sparse.randn(data_size, 1, sparsity_level))
 y = Phi@x0
-P_rand = sparse.rand_plane(data_size) # A random plane onto which we'll project the data
 
-# We do functions which computes and display the regularization path 
+
 def compute_reg_path(Phi, y, reg_param_grid):
-    reg_path = np.empty((Phi.shape[1],0), int)
-    x_ini = np.zeros( (Phi.shape[1],1) )
+    # We do functions which computes and display the regularization path
+    reg_path = np.empty((Phi.shape[1], 0), int)
+    x_ini = np.zeros((Phi.shape[1], 1))
     for reg_param in reg_param_grid:
-        # We choose the number of iterations to do depending on the reg_param. This is a completely custom choice, it seems to work quite well on random problems.
+        ''' We choose the number of iterations to do depending on the reg_param.
+        This is a completely custom choice, it seems to work quite well
+        on random problems.
+        '''
         if reg_param < 0.1:
-            iter_nb=40000
+            iter_nb = 40000
         elif reg_param < 1:
-            iter_nb=4000
+            iter_nb = 4000
         elif reg_param < 10:
             iter_nb = 1000
         else:
             iter_nb = 200
-        # We use a warm restart approach: for each problem we use the solution of the previous problem as a starting point
+        # We use a warm restart approach:
+        # for each problem we use the solution of the previous problem
+        # as a starting point
         x_reg = fb.lasso(Phi, y, reg_param, iter_nb, x_ini=x_ini)
         x_ini = x_reg
-        reg_path = np.concatenate((reg_path,x_reg), axis=1)
+        reg_path = np.concatenate((reg_path, x_reg), axis=1)
     return reg_path
 
-def scatter_reg_path(path,limit=None,title=None):
+
+def scatter_reg_path(path, limit=None, title=None):
     cm = plt.cm.get_cmap('RdYlBu')
     if limit is not None:
-        plt.scatter(limit[0,:],limit[1,:],c='r',marker='x',s=150)
+        plt.scatter(limit[0, :], limit[1, :], c='r', marker='x', s=150)
     if title is not None:
         _ = plt.title(title)
-    plt.plot(path[0,:],path[1,:], c='black',linewidth=0.5)
-    fig = plt.scatter(path[0,:], path[1,:], c=range(path.shape[1]), vmin=0, vmax=path.shape[1],  s=35, cmap=cm)
+    plt.plot(path[0, :], path[1, :], c='black', linewidth=0.5)
+    fig = plt.scatter(path[0, :], path[1, :], c=range(path.shape[1]),
+                      vmin=0, vmax=path.shape[1], s=35, cmap=cm)
     plt.colorbar(fig)
 
-# We put a grid on the reg_param, by taking them on a log scale and decreasing order
-regp_min=-2
-regp_max=2
-regp_number=100
-reg_param_grid = np.round(np.logspace(regp_min,regp_max,regp_number),3)[::-1]
+# We put a grid on the reg_param, by taking it on a log scale and decreasing
+regp_min = -2
+regp_max = 2
+regp_number = 100
+reg_param_grid = \
+    np.round(np.logspace(regp_min, regp_max, regp_number), 3)[::-1]
 
 # Let's compute the regularization path for various values of noise
-noisy_vector = np.random.randn(data_number,1)
+noisy_vector = np.random.randn(data_number, 1)
 reg_path = compute_reg_path(Phi, y, reg_param_grid)
 reg_path_0 = compute_reg_path(Phi, y + 1e-0 * noisy_vector, reg_param_grid)
 reg_path_1 = compute_reg_path(Phi, y + 1e-1 * noisy_vector, reg_param_grid)
 reg_path_2 = compute_reg_path(Phi, y + 1e-2 * noisy_vector, reg_param_grid)
 
 # We represent the data on a plane. Let's use SVD on the noisier reg path.
-# The results look less weird and ressembles the reg path in the quadratic case.
-centered_points = reg_path_0 - np.mean(reg_path_0,axis=1).reshape(reg_path_0.shape[0],1)
+# The results ressembles the reg path in the quadratic case.
+centered_points = reg_path_0 \
+    - np.mean(reg_path_0, axis=1).reshape(reg_path_0.shape[0], 1)
 V = np.cov(centered_points)
 _, vectors = la.eig(V)
-P_svd = np.real(vectors[:,0:2]).T
+P_svd = np.real(vectors[:, 0:2]).T
 
 plt.figure(dpi=dpi)
 plt.subplot(2, 2, 1)
-scatter_reg_path(P_svd@reg_path,limit=P_svd@x0,title="$\sigma=0$")
+scatter_reg_path(P_svd@reg_path, limit=P_svd@x0, title="$\sigma=0$")
 plt.subplot(2, 2, 2)
-scatter_reg_path(P_svd@reg_path_2,limit=P_svd@x0,title="$\sigma=0.01$")
+scatter_reg_path(P_svd@reg_path_2, limit=P_svd@x0, title="$\sigma=0.01$")
 plt.subplot(2, 2, 3)
-scatter_reg_path(P_svd@reg_path_1,limit=P_svd@x0,title="$\sigma=0.1$")
+scatter_reg_path(P_svd@reg_path_1, limit=P_svd@x0, title="$\sigma=0.1$")
 plt.subplot(2, 2, 4)
-scatter_reg_path(P_svd@reg_path_0,limit=P_svd@x0,title="$\sigma=1$")
+scatter_reg_path(P_svd@reg_path_0, limit=P_svd@x0, title="$\sigma=1$")
 plt.show()
 
-# Parameter selection
-def reg_param_selection(path,ground_truth):
-    # Given a regularization path and a ground truth, returns the best regularized solution (in L2 sense)
+
+def reg_param_selection(path, ground_truth):
+    '''Given a regularization path and a ground truth,
+    returns the best regularized solution (in L2 sense)
+    '''
     path_length = path.shape[1]
     path_dimension = path.shape[0]
-    reg_sol = path[:,0].reshape(path_dimension, 1)
+    reg_sol = path[:, 0].reshape(path_dimension, 1)
     reg_param_index = 0
     for k in np.arange(path_length):
-        print(la.norm(ground_truth.T - path[:,k]))
-        if la.norm(ground_truth.T - path[:,k]) < la.norm(ground_truth.T - reg_sol):
-            reg_sol = path[:,k].reshape(path_dimension, 1)
+        print(la.norm(ground_truth.T - path[:, k]))
+        if la.norm(ground_truth.T - path[:, k]) \
+                < la.norm(ground_truth.T - reg_sol):
+            reg_sol = path[:, k].reshape(path_dimension, 1)
             reg_param_index = k
     return reg_sol, reg_param_index
 
 # We compute the best parameter given the ground truth
-reg_sol, reg_param_index = reg_param_selection(reg_path,x0)
-reg_sol_2, reg_param_index_2 = reg_param_selection(reg_path_2,x0)
-reg_sol_1, reg_param_index_1 = reg_param_selection(reg_path_1,x0)
-reg_sol_0, reg_param_index_0 = reg_param_selection(reg_path_0,x0)
+reg_sol, reg_param_index = reg_param_selection(reg_path, x0)
+reg_sol_2, reg_param_index_2 = reg_param_selection(reg_path_2, x0)
+reg_sol_1, reg_param_index_1 = reg_param_selection(reg_path_1, x0)
+reg_sol_0, reg_param_index_0 = reg_param_selection(reg_path_0, x0)
 
 # We represent the selected solutions
 _ = plt.figure(dpi=dpi)
 plt.subplot(2, 2, 1)
-sparse.stem(x0,"C0","ground truth")
-sparse.stem(reg_sol,"C1","reg solution")
+sparse.stem(x0, "C0", "ground truth")
+sparse.stem(reg_sol, "C1", "reg solution")
 plt.title("$\sigma=0$")
 plt.subplot(2, 2, 2)
-sparse.stem(x0,"C0","ground truth")
-sparse.stem(reg_sol_2,"C1","reg solution")
+sparse.stem(x0, "C0", "ground truth")
+sparse.stem(reg_sol_2, "C1", "reg solution")
 plt.title("$\sigma=0.01$")
 plt.subplot(2, 2, 3)
-sparse.stem(x0,"C0","ground truth")
-sparse.stem(reg_sol_1,"C1","reg solution")
+sparse.stem(x0, "C0", "ground truth")
+sparse.stem(reg_sol_1, "C1", "reg solution")
 plt.title("$\sigma=0.1$")
 plt.subplot(2, 2, 4)
-sparse.stem(x0,"C0","ground truth")
-sparse.stem(reg_sol_0,"C1","reg solution")
+sparse.stem(x0, "C0", "ground truth")
+sparse.stem(reg_sol_0, "C1", "reg solution")
 plt.title("$\sigma=1$")
 plt.show()
 
 plt.figure(dpi=dpi)
 plt.subplot(2, 2, 1)
-scatter_reg_path(P_svd@reg_path,limit=P_svd@x0,title="$\sigma$=0, $\lambda$="+str(reg_param_grid[reg_param_index]))
+scatter_reg_path(P_svd@reg_path, limit=P_svd@x0,
+                 title="$\sigma$=0, $\lambda$=" +
+                 str(reg_param_grid[reg_param_index]))
 plt.subplot(2, 2, 2)
-scatter_reg_path(P_svd@reg_path_2,limit=P_svd@x0,title="$\sigma$=0.01, $\lambda$="+str(reg_param_grid[reg_param_index_2]))
+scatter_reg_path(P_svd@reg_path_2, limit=P_svd@x0,
+                 title="$\sigma$=0.01, $\lambda$=" +
+                 str(reg_param_grid[reg_param_index_2]))
 plt.subplot(2, 2, 3)
-scatter_reg_path(P_svd@reg_path_1,limit=P_svd@x0,title="$\sigma$=0.1, $\lambda$="+str(reg_param_grid[reg_param_index_1]))
+scatter_reg_path(P_svd@reg_path_1, limit=P_svd@x0,
+                 title="$\sigma$=0.1, $\lambda$=" +
+                 str(reg_param_grid[reg_param_index_1]))
 plt.subplot(2, 2, 4)
-scatter_reg_path(P_svd@reg_path_0,title="$\sigma$=1, $\lambda$="+str(reg_param_grid[reg_param_index_0]),\
-    limit=np.concatenate((P_svd@x0,P_svd@reg_sol_0), axis=1))
+scatter_reg_path(P_svd@reg_path_0,
+                 title="r $\sigma$=1, $\lambda$=" +
+                 str(reg_param_grid[reg_param_index_0]),
+                 limit=np.concatenate((P_svd@x0, P_svd@reg_sol_0), axis=1))
 plt.show()
 
 
@@ -150,7 +169,7 @@ plt.show()
 
 
 ####################################################
-############ OLD ###################################
+# OLD
 ####################################################
 
 """
