d1d2f83b6a16a64f93b541250aac9f7622430d5c
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2019-03-12 18:28:45 +0100

    Jupyter notebook: Lasso 1

--- my_toolbox/forwardbackward.py
+++ my_toolbox/forwardbackward.py
@@ -4,8 +4,12 @@ import my_toolbox.sparsity as sparse
 
 
 def lasso(A, y, reg_param, iter_nb, x_ini=None, verbose=False):
-    # Use the Forward-Backward algorithm to find a minimizer of:
-    # reg_param*norm(x,1) + 0.5*norm(Ax-y,2)**2
+    ''' Use the Forward-Backward algorithm to find a minimizer of:
+             reg_param*norm(x,1) + 0.5*norm(Ax-y,2)**2
+        Eventually outputs the functional values and support of the iterates 
+        while running the method
+    '''
+    # Manage optional input/output
     if verbose:  # Optional output
         regret = np.zeros(iter_nb)
         support = np.zeros(iter_nb)
@@ -17,6 +21,8 @@ def lasso(A, y, reg_param, iter_nb, x_ini=None, verbose=False):
         x = x_ini
     else:
         x = np.zeros((A.shape[1], 1))
+
+    # The core of the algorithm
     stepsize = 0.8 * 2/la.norm(A, 2)**2
     for k in range(iter_nb):
         x = x - stepsize * A.T@(A@x - y)
@@ -24,6 +30,8 @@ def lasso(A, y, reg_param, iter_nb, x_ini=None, verbose=False):
         if verbose:
             regret[k] = 0.5*la.norm(A@x-y, 2)**2 + reg_param*la.norm(x, 1)
             support[k] = sparse.norm0(x)
+    
+    # Output
     if verbose:
         return x, details
     else:
