39768bbaee423baa4098f84a5aebc9908eddbde5
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2019-03-12 20:41:51 +0100

    Replace - by _ for package install

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-        name="inverse-problems",
+        name="inverse_problems",
         version="0.0.1",
         author="guillaume garrigos",
         author_email="guillaume.garrigos@lpsm.paris",
-        packages=["inverse-problems"],
-        package_dir={"inverse-problems":"inverse-problems"},
-        url="https://github.com/Guillaume-Garrigos/inverse-problems",
+        packages=["inverse_problems"],
+        package_dir={"inverse_problems":"inverse_problems"},
+        url="https://github.com/Guillaume-Garrigos/inverse_problems",
         license="MIT",
         install_requires=["numpy >= 1.8"]
 )
