46d289ea61aaba4d00a6687b5367b4107f2bd9da
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2019-03-12 23:06:12 +0100

    Create a invprob package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-        name="inverse_problems",
-        version="0.0.3",
+        name="invprob",
+        version="0.0.1",
         author="Guillaume Garrigos",
-        author_email="guillaume.garrigos@gmail.com",
-        packages=["inverseproblemspkg"],
-        package_dir={"inverseproblemspkg":"inverseproblemspkg"},
-        url="https://github.com/Guillaume-Garrigos/inverse_problems/",
+        author_email="guillaume.garrigos@lpsm.paris",
+        packages=["invprob"],
+        package_dir={"invprob":"invprob"},
+        url="https://github.com/Guillaume-Garrigos/invprob/",
         license="MIT",
         install_requires=["numpy >= 1.9"]
 )
