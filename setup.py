7b8befb648cb56b99dcdbf5cb66a7a4aa71cde18
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2019-03-12 22:24:38 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
         name="inverse_problems",
-        version="0.0.1",
-        author="guillaume garrigos",
+        version="0.0.2",
+        author="Guillaume Garrigos",
         author_email="guillaume.garrigos@lpsm.paris",
         packages=["inverse_problems"],
-        package_dir={"inverse_problems": "inverse_problems"},
+        package_dir={"inverse_problems":"inverse_problems"},
         url="https://github.com/Guillaume-Garrigos/inverse_problems/",
         license="MIT",
-        install_requires=["numpy >= 1.8"]
+        install_requires=["numpy >= 1.9"]
 )
