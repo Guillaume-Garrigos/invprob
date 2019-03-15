19d73a1c2f38d1186ece53a729c6c081fa3820a9
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2019-03-15 18:37:17 +0100

    Image dataset

---
+++ scripts/imaging_1-denoising_deblurring.py
@@ -0,0 +1,10 @@
+import numpy as np
+from numpy import linalg as la
+import matplotlib.pyplot as plt
+import os.path
+
+import sys
+sys.path.append('..')
+import invprob.sparse as sparse
+from invprob.optim import fb_lasso
+
