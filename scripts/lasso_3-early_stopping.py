ccc308d8eb28636a7aff2b1a5011630962ee1072
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2019-05-10 16:23:19 -0400

    Diagonal FB early stopping works

--- scripts/lasso_3-early_stopping.py
+++ scripts/lasso_3-early_stopping.py
@@ -8,7 +8,7 @@ from invprob.optim import fb_lasso
 
 
 
-np.random.seed(seed=78)  # Seed for np.random (78)
+np.random.seed(seed=74)  # Seed for np.random (74)
 dpi = 100  # Resolution for plotting (230 for small screen, 100 for large one)
 plt.ion()
 folder = "scripts/../output/L1_reg/"
@@ -34,29 +34,48 @@ _ = plt.figure(dpi=dpi)
 sparse.stem(x0, "C0", "ground truth")
 sparse.stem(x_sol, "C1", "inverse solution")
 
+# We compute the best parameter given the ground truth
+def reg_param_selection(path, ground_truth):
+    '''Given a regularization path and a ground truth,
+    returns the best regularized solution (in L2 sense)
+    '''
+    path_length = path.shape[1]
+    reg_sol = path[:, 0, None]
+    reg_param_index = 0
+    for k in np.arange(path_length):
+        if la.norm(ground_truth - path[:, k, None]) \
+                < la.norm(ground_truth - reg_sol):
+            reg_sol = path[:, k, None]
+            reg_param_index = k
+    return reg_sol, reg_param_index
+
 
 # We solve the noisy problem
-y = Phi@x0 + 0.1 * noisy_vector
-iter_nb = 10000
+y = Phi@x0 + 0.5 * noisy_vector
+iter_nb = 100000
 exp_decay = 0.1  # The smaller the exponent, the faster is the algorithm
 reg_param_grid = 1 / (np.arange(iter_nb)+1)**exp_decay
 
+x_sol2 = fb_lasso(Phi, y, 0.001, iter_nb=100000)
+x_reg2 = fb_lasso(Phi, y, 0.87, iter_nb=10000)
+
 x_sol, details = fb_lasso(Phi, y, reg_param_grid, iter_nb, verbose=True)
-
-
+reg_path = details["iterate_path"]
+x_reg, reg_param_index = reg_param_selection(reg_path, x0)
 
 _ = plt.figure(dpi=dpi)
 sparse.stem(x0, "C0", "ground truth")
-sparse.stem(x_sol, "C1", "reg solution")
+sparse.stem(x_sol, "C2", "inv diag solution")
+sparse.stem(x_sol2, "C1", "inv tikh solution")
+
+_ = plt.figure(dpi=dpi)
+sparse.stem(x0, "C0", "ground truth")
+sparse.stem(x_reg, "C2", "reg diag solution")
+sparse.stem(x_reg2, "C1", "reg tikh solution")
 
 
 
 
-sparse.stem(details["iterate_path"][:, 10], "C1", "reg solution")
-sparse.stem(details["iterate_path"][:, 100], "C2", "reg solution")
-sparse.stem(details["iterate_path"][:, 1000], "C3", "reg solution")
-sparse.stem(details["iterate_path"][:, 10000], "C4", "reg solution")
-sparse.stem(details["iterate_path"][:, 99999], "C5", "reg solution")
 
 
 #########################################
